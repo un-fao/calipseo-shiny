7b559497c167abb3e57921959424268faa0ba30a
Ojok Brian Andrew <46162824+brianandy@users.noreply.github.com>
2021-11-10 14:52:22 +0300

    vessel register improved

--- shiny/shiny-calipseo/modules/core/vessel_list_server.R
+++ shiny/shiny-calipseo/modules/core/vessel_list_server.R
@@ -25,12 +25,17 @@ vessel_list_server <- function(input, output, session, pool) {
   
   df <- reactiveValues(data = outp)
   
+  df <- df$data %>% dplyr::select(-c(VESSEL_TYPE_CODE,VESSEL_OPERATIONAL_STATUS_CODE,VESSEL_STAT_TYPE_CODE,
+                                     VESSEL_STAT_TYPE,REG_PORT_LANDING_SITE,VESSEL_OPERATIONAL_STATUS))
+  
+  df <- dplyr::rename(df,'REGISTRATION NUMBER'= REGISTRATION_NUMBER, "VESSEL TYPE"= VESSEL_TYPE, "HOME PORT/LANDING SITE"=HOME_PORT_LANDING_SITE)
+  
   output$vessel_list <- renderDataTable(
-    df$data,
+    df,
     server = FALSE,
     escape = FALSE,
     rownames = FALSE,
-    extensions = c("Buttons"), 
+    extensions = c("Buttons"),
     options = list(
       autoWidth = TRUE,
       dom = 'Bfrtip',
@@ -44,8 +49,12 @@ vessel_list_server <- function(input, output, session, pool) {
       ),
       exportOptions = list(
         modifiers = list(page = "all", selected = TRUE)
-      )
+      ),
+      columnDefs = list(list(searchable = FALSE,targets=6)),
+      pageLength = 5
     ),
     filter = list(position = 'top', clear = FALSE))
+
+
    
 }
