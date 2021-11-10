796811fc9e68c7ce4de715a58fa6d188b50da822
Ojok Brian Andrew <46162824+brianandy@users.noreply.github.com>
2021-11-10 11:33:04 +0300

    features have been added but you MUST add the reshape2 library to the yml configuration file of each country.

--- shiny/shiny-calipseo/modules/core/vessel_breakdown_server.R
+++ shiny/shiny-calipseo/modules/core/vessel_breakdown_server.R
@@ -1,6 +1,8 @@
 #vessel_breakdown_server
 vessel_breakdown_server <- function(input, output, session, pool) {
   
+ 
+  
   output$vessel_breakdown_info <- renderText({
     session$userData$page("vessel-breakdown")
     updatePageUrl("vessel-breakdown", session)
@@ -11,14 +13,48 @@ vessel_breakdown_server <- function(input, output, session, pool) {
   #vessels breakdown by type (pie chart)
   output$rep_vessels <- renderPlotly({
     vessel_breakdown = accessVesselsCountByType(pool)
+    
     plot_ly(vessel_breakdown, labels = ~NAME, values = ~COUNT, type = 'pie', sort = FALSE, direction = "clockwise") %>%
       layout(xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
              yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE))
+    
   })
+  
+  
+  df_vessel_type <- accessVesselsCountByType(pool)[,-1] %>% 
+    dplyr::rename("VESSEL TYPE"= NAME)
+  
+  output$rep_vessels_data <- renderDataTable(
+    df_vessel_type,
+    server = FALSE,
+    escape = FALSE,
+    rownames = FALSE,
+    extensions = c("Buttons"), 
+    options = list(
+      autoWidth = TRUE,
+      dom = 'Bfrtip',
+      deferRender = TRUE,
+      scroll = FALSE,
+      buttons = list(
+        list(extend = 'copy'),
+        list(extend = 'csv', filename =  paste("Vessel Type Data Breakdown in",appConfig$country_profile$country), title = NULL, header = TRUE),
+        list(extend = 'excel', filename =  paste("Vessel Type Data Breakdown in",appConfig$country_profile$country), title = NULL, header = TRUE),
+        list(extend = "pdf", title = paste("Vessel Type Data Breakdown in",appConfig$country_profile$country), header = TRUE, orientation = "landscape")
+      ),
+      exportOptions = list(
+        modifiers = list(page = "all", selected = TRUE)
+      )
+    ),
+    filter = list(position = 'top', clear = FALSE))
+  
+  
+  
+  
+  
   
   output$map_vessels <- renderLeaflet({
     
-    sites_vessels <- accessVesselsCountByLandingSite(pool) 
+    sites_vessels <- accessVesselsCountByLandingSite(pool)
     leaflet() %>%
       addProviderTiles(providers$Esri.OceanBasemap, options = providerTileOptions(noWrap = TRUE)) %>%  
       addCircles(data = sites_vessels, weight = 1, color = "blue", fillColor = "blue", fillOpacity = 0.7, 
@@ -29,4 +65,114 @@ vessel_breakdown_server <- function(input, output, session, pool) {
                  ))
   })
   
+  
+  
+  df_vessel_landingsite_data <- as.data.frame(accessVesselsCountByLandingSite(pool))[,-3] %>% 
+    dplyr::rename("HOME PORT"= NAME)
+  
+  
+  output$map_vessel_data <- renderDataTable(
+    df_vessel_landingsite_data,
+    server = FALSE,
+    escape = FALSE,
+    rownames = FALSE,
+    extensions = c("Buttons"), 
+    options = list(
+      autoWidth = TRUE,
+      dom = 'Bfrtip',
+      deferRender = TRUE,
+      scroll = FALSE,
+      buttons = list(
+        list(extend = 'copy'),
+        list(extend = 'csv', filename =  paste("Vessel LandingSite Data Breakdown in",appConfig$country_profile$country), title = NULL, header = TRUE),
+        list(extend = 'excel', filename = paste("Vessel LandingSite Data Breakdown in",appConfig$country_profile$country), title = NULL, header = TRUE),
+        list(extend = "pdf", title = paste("Vessel LandingSite Data Breakdown in",appConfig$country_profile$country), header = TRUE, orientation = "landscape")
+      ),
+      exportOptions = list(
+        modifiers = list(page = "all", selected = TRUE)
+      )
+    ),
+    filter = list(position = 'top', clear = FALSE))
+  
+  
+  output$rep_vessels_home_port <- renderPlotly({
+    
+    plot_ly(df_vessel_landingsite_data, labels = ~`HOME PORT`, values = ~COUNT, type = 'pie', sort = FALSE, direction = "clockwise") %>%
+      layout(xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
+             yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE))
+    
+    
+  })
+  
+  
+
+  df_vessel_landingsite_data_breakdown <- accessVessels(pool) %>% group_by(HOME_PORT_LANDING_SITE,VESSEL_TYPE) %>%
+    count(name = 'COUNT') %>%
+    dplyr::rename("VESSEL TYPE"= VESSEL_TYPE, "HOME PORT/LANDING SITE"=HOME_PORT_LANDING_SITE) %>%
+    dplyr::filter(`HOME PORT/LANDING SITE`!="Unknown")
+
+ 
+
+  output$map_vessel_data_breakdown <- renderDataTable(
+    df_vessel_landingsite_data_breakdown[,c(1:3)],
+    server = FALSE,
+    escape = FALSE,
+    rownames = FALSE,
+    extensions = c("Buttons"),
+    options = list(
+      autoWidth = TRUE,
+      dom = 'Bfrtip',
+      deferRender = TRUE,
+      scroll = FALSE,
+      buttons = list(
+        list(extend = 'copy'),
+        list(extend = 'csv', filename =  paste("Home port/LandingSite Vessel Data Breakdown in",appConfig$country_profile$country), title = NULL, header = TRUE),
+        list(extend = 'excel', filename = paste("Home port/LandingSite Vessel Data Breakdown in",appConfig$country_profile$country), title = NULL, header = TRUE),
+        list(extend = "pdf", title = paste("Home port/LandingSite Vessel Data Breakdown in",appConfig$country_profile$country), header = TRUE, orientation = "landscape")
+      ),
+      exportOptions = list(
+        modifiers = list(page = "all", selected = TRUE)
+      )
+    ),
+    filter = list(position = 'top', clear = FALSE))
+  
+  
+  
+  
+  vessel_landingsite_breakdown <- accessVessels(pool) %>% select(c('HOME_PORT_LANDING_SITE','LONGITUDE','LATITUDE','VESSEL_TYPE', )) %>%
+    dplyr::filter(LONGITUDE!="",LATITUDE!="") %>%
+    group_by(HOME_PORT_LANDING_SITE,VESSEL_TYPE,LONGITUDE,LATITUDE) %>%
+    count(name = 'COUNT') %>%
+    dplyr::rename("VESSEL TYPE"= VESSEL_TYPE, "HOME PORT/LANDING SITE"=HOME_PORT_LANDING_SITE) %>%
+    dplyr::filter(`HOME PORT/LANDING SITE`!="Unknown")
+  
+
+  vessel_landingsite_breakdown <- reshape2::dcast(vessel_landingsite_breakdown, LONGITUDE+LATITUDE+`HOME PORT/LANDING SITE`~`VESSEL TYPE`,value.var = "COUNT")
+  
+  vessel_landingsite_breakdown <- vessel_landingsite_breakdown %>% mutate(Total=rowSums(vessel_landingsite_breakdown[,4:ncol(vessel_landingsite_breakdown)], na.rm = T))
+  
+
+  vessel_landingsite_breakdown$LONGITUDE <- as.numeric(vessel_landingsite_breakdown$LONGITUDE)
+  vessel_landingsite_breakdown$LATITUDE <- as.numeric(vessel_landingsite_breakdown$LATITUDE)
+
+
+  output$map_vessels2 <- renderLeaflet({
+
+
+    leaflet() %>%
+      addProviderTiles(providers$Esri.OceanBasemap, options = providerTileOptions(noWrap = TRUE)) %>%
+      addCircles(data = vessel_landingsite_breakdown, lat = vessel_landingsite_breakdown$LATITUDE, lng = vessel_landingsite_breakdown$LONGITUDE,weight = 1, color = "blue", fillColor = "blue", fillOpacity = 0.7
+      ) %>%
+      addMinicharts(lng = vessel_landingsite_breakdown$LONGITUDE,
+                    lat = vessel_landingsite_breakdown$LATITUDE,
+                    type = "pie",
+                    legendPosition = 'bottomright',
+                    chartdata = vessel_landingsite_breakdown[,c(4:(ncol(vessel_landingsite_breakdown)-1))],
+                    width = 40 * sqrt(vessel_landingsite_breakdown$Total) / sqrt(max(vessel_landingsite_breakdown$Total)), transitionTime = 0
+
+                    )
+  })
+
+  
 }
+
--- shiny/shiny-calipseo/modules/core/vessel_breakdown_ui.R
+++ shiny/shiny-calipseo/modules/core/vessel_breakdown_ui.R
@@ -11,8 +11,38 @@ vessel_breakdown_ui <- function(id){
             )
           ),
           fluidRow(
-            box(width = 6, height = 540, title = sprintf("Breakdown of vessels in %s per vessel type", appConfig$country_profile$country), status = "primary", solidHeader= TRUE, plotlyOutput(ns("rep_vessels"))),
-            box(width = 6, height = 540, title = sprintf("Breakdown of vessels in %s per home port", appConfig$country_profile$country), status = "primary", solidHeader= TRUE, leafletOutput(ns("map_vessels"), height = 490))
+            
+            box(width = 6, height = 480, title = sprintf("Breakdown of vessels in %s per vessel type", appConfig$country_profile$country), status = "primary", solidHeader= TRUE, plotlyOutput(ns("rep_vessels"))),
+            
+            box(width = 6, height = 480, title = sprintf("Data on Breakdown of vessels in %s per vessel type", appConfig$country_profile$country), status = "primary", solidHeader= TRUE, DT::dataTableOutput(ns("rep_vessels_data")))
+          ),
+          fluidRow(
+            
+            box(width = 6, height = 610, title = sprintf("Breakdown of vessels in %s per home port", appConfig$country_profile$country), status = "primary", solidHeader= TRUE,
+            
+              
+              tabsetPanel(
+                id = "breakdown_vessel_tab", type = "pills",
+                tabPanel(title = "Map", leafletOutput(ns("map_vessels"), height = 450)),
+                tabPanel(title = "Data", tags$div(DTOutput(ns("map_vessel_data"))), style = "margin:6px;")
+              )
+            
+            
+            ),
+            
+            box(width = 6, height = 610, title = sprintf("Breakdown of vessels in %s per home port", appConfig$country_profile$country), status = "primary", solidHeader= TRUE,
+                
+                tabsetPanel(
+                  id = "breakdown_vessel_tab2", type = "pills",
+                  tabPanel(title = "Map", leafletOutput(ns("map_vessels2"), height = 450)),
+                  tabPanel(title = "Data", tags$div(DTOutput(ns("map_vessel_data_breakdown"))), style = "margin:6px;")
+                )#,
+                
+                #plotlyOutput(ns("rep_vessels_home_port"))
+                
+                
+                )
+
           )
   )
   
