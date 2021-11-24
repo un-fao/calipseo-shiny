02fa943318a06586811c0d6310b6896eb4aea685
Bennici <alexandre-ben@hotmail.fr>
2021-11-24 18:30:23 +0100

    add 3 new plot to display quantity of fish catch by vessel type, species and fish group with capacity to manage temporal resolution and statistic

--- shiny/shiny-calipseo/modules/core/logbooks_overview_ui.R
+++ shiny/shiny-calipseo/modules/core/logbooks_overview_ui.R
@@ -29,7 +29,61 @@ logbooks_overview_ui <- function(id){
                 DT::dataTableOutput(ns("stats_by_type_currentyear_table"))
               )
             )
-          )
+          ),
+          fluidRow(
+            div(
+              class = "col-md-6",
+              h3(sprintf("Overview of quantity catched by vessel type")), hr(),
+              box(
+                title="",
+                width = 12,
+                sidebar = shinydashboardPlus::boxSidebar(
+                  id=ns("vt_sidebar"),
+                  width = 25,
+                  style = 'font-size:14px;',
+                selectInput(ns("vt_granu"),"Temporal resolution :",choices=c("Yearly"="%Y","Monthly"="%Y-%m","Weekly"="%Y-%U")),
+                selectInput(ns("vt_stat"),"Statistic :",choices=c("Total"="sum","Average"="mean","Median"="median")),
+                uiOutput(ns("vt_additional"))),
+                plotlyOutput(ns("vt_plot")))
+                
+              ),
+            div(
+              class = "col-md-6",
+              h3(sprintf("Overview of quantity catched by species"), hr(),
+                 box(
+                   title="",
+                   width = 12,
+                   sidebar = shinydashboardPlus::boxSidebar(
+                     id=ns("sp_sidebar"),
+                     width = 30,
+                     style = 'font-size:14px;',
+                     uiOutput(ns("sp_nb_selector")),
+                     selectInput(ns("sp_rank_method"),"Rank method :",choices=c("Total catch over the period"="sum","Last year total catch"="last_year","Annual catch average"="year_avg")),
+                     selectInput(ns("sp_granu"),"Temporal resolution :",choices=c("Yearly"="%Y","Monthly"="%Y-%m","Weekly"="%Y-%U")),
+                     selectInput(ns("sp_stat"),"Statistic :",choices=c("Total"="sum","Average"="mean","Median"="median")),
+                     uiOutput(ns("sp_additional"))),
+                   plotlyOutput(ns("sp_plot")))
+              )
+            )
+          ),
+        fluidRow(
+          div(
+            class = "col-md-6",
+            h3(sprintf("Overview of quantity catched by fish group")), hr(),
+              box(
+                title="",
+                width = 12,
+                sidebar = shinydashboardPlus::boxSidebar(
+                  id=ns("fg_sidebar"),
+                  width = 25,
+                  style = 'font-size:14px;',
+                  selectInput(ns("fg_granu"),"Temporal resolution :",choices=c("Yearly"="%Y","Monthly"="%Y-%m","Weekly"="%Y-%U")),
+                  selectInput(ns("fg_stat"),"Statistic :",choices=c("Total"="sum","Average"="mean","Median"="median")),
+                  uiOutput(ns("fg_additional"))),
+                plotlyOutput(ns("fg_plot")))
+            )
+        )
+          
   )
   
 }
