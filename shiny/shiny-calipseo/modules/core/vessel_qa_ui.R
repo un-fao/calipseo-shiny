13a026a78f7c7cf3213100cca62dfe03fcfac53d
Ojok Brian Andrew <46162824+brianandy@users.noreply.github.com>
2022-04-06 12:17:23 +0300

    vessel qa ports switched to single table

--- shiny/shiny-calipseo/modules/core/vessel_qa_ui.R
+++ shiny/shiny-calipseo/modules/core/vessel_qa_ui.R
@@ -14,7 +14,7 @@ vessel_qa_ui <- function(id){
           div(class='row',style = "margin:0px;", id='vertical-panels-vessel-qa',
               shinyWidgets::verticalTabsetPanel(
                 shinyWidgets::verticalTabPanel(i18n("VERTICALTABPANEL_VESSEL_QA_NAMES"),box_height='70px',box(width = 12, height = 480, title = i18n("VESSEL_QA_TITLE_NAMES"),DT::dataTableOutput(ns("vessel_names")))) ,
-                shinyWidgets::verticalTabPanel(i18n("VERTICALTABPANEL_VESSEL_QA_PORTS"),box_height='70px' ,fluidRow(box(width = 12, height = 480, title = i18n("VESSEL_QA_TITLE_HOMEPORT"), DT::dataTableOutput(ns("vessel_homeport")))),fluidRow(box(width = 12, height = 480, title = i18n("VESSEL_QA_TITLE_REGPORT"),DT::dataTableOutput(ns("vessel_regport")))),sliderWidth =25),
+                shinyWidgets::verticalTabPanel(i18n("VERTICALTABPANEL_VESSEL_QA_PORTS"),box_height='70px' ,box(width = 12, height = 480, title = i18n("VESSEL_QA_TITLE_PORTS"), DT::dataTableOutput(ns("vessel_ports"))),sliderWidth =25),
                 shinyWidgets::verticalTabPanel(i18n("VERTICALTABPANEL_VESSEL_QA_CHARACTERISTICS"),box_height='70px',box(width = 12, height = 480, title = i18n("VESSEL_QA_TITLE_CHARACTERISTICS"),DT::dataTableOutput(ns("vessel_characteristics"))))))
   )
   
