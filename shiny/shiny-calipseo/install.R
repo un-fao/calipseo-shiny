af08ed7934b1a097980696efe03756c8bd9bc50a
eblondelfao <emmanuel.blondel@fao.org>
2022-04-08 09:09:10 +0200

    work on docker/package installation

--- shiny/shiny-calipseo/install.R
+++ shiny/shiny-calipseo/install.R
@@ -1,5 +1,3 @@
-#!/usr/bin/env r
-
 package <- jsonlite::read_json('./srv/shiny-server/package.json')
 invisible(lapply(package$dependencies, function(pkg){
   from <- 'cran'
@@ -11,7 +9,7 @@ invisible(lapply(package$dependencies, function(pkg){
   if(class(pkg_installer)[1] == "try-error") return(NULL)
   version <- ""
   if(!is.null(pkg$version)) version <- paste0("[",pkg$version,"]")
-  cat(sprintf("Install package '%s' %s from '%s'\n", pkg$package, version, from))
+  cat(sprintf("Install package '%s' %s from '%s'\n", pkg$package, version, from), file = stderr())
   pkg_args <- pkg[names(pkg)!="from"]
   do.call(pkg_installer, pkg_args)
 }))
