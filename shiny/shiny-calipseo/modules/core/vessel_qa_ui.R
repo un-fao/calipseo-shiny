a4dc65f59ff6ee017b574e92fe0519b847b45470
Ojok Brian Andrew <46162824+brianandy@users.noreply.github.com>
2022-04-08 11:09:24 +0300

    vessel_qa operational status tab added

--- shiny/shiny-calipseo/modules/core/vessel_qa_ui.R
+++ shiny/shiny-calipseo/modules/core/vessel_qa_ui.R
@@ -16,7 +16,8 @@ vessel_qa_ui <- function(id){
                 shinyWidgets::verticalTabPanel(i18n("VERTICALTABPANEL_VESSEL_QA_NAMES"),box_height='70px',box(width = 12, height = 480, title = i18n("VESSEL_QA_TITLE_NAMES"),DT::dataTableOutput(ns("vessel_names")))) ,
                 shinyWidgets::verticalTabPanel(i18n("VERTICALTABPANEL_VESSEL_QA_PORTS"),box_height='70px' ,box(width = 12, height = 480, title = i18n("VESSEL_QA_TITLE_PORTS"), DT::dataTableOutput(ns("vessel_ports"))),sliderWidth =25),
                 shinyWidgets::verticalTabPanel(i18n("VERTICALTABPANEL_VESSEL_QA_CHARACTERISTICS"),box_height='70px',box(width = 12, height = 480, title = i18n("VESSEL_QA_TITLE_CHARACTERISTICS"),DT::dataTableOutput(ns("vessel_characteristics")))),
-                shinyWidgets::verticalTabPanel(i18n("VERTICALTABPANEL_VESSEL_QA_LICENSE"),box_height='70px',box(width = 12, height = 480, title = i18n("VESSEL_QA_TITLE_LICENSE"),DT::dataTableOutput(ns("vessel_license"))))))
+                shinyWidgets::verticalTabPanel(i18n("VERTICALTABPANEL_VESSEL_QA_LICENSE"),box_height='70px',box(width = 12, height = 480, title = i18n("VESSEL_QA_TITLE_LICENSE"),DT::dataTableOutput(ns("vessel_license")))),
+                shinyWidgets::verticalTabPanel(i18n("VERTICALTABPANEL_VESSEL_QA_VOP_STATUS"),box_height='70px',box(width = 12, height = 480, title = i18n("VESSEL_QA_TITLE_VOP_STATUS"),DT::dataTableOutput(ns("vessel_operational_status"))))))
   )
   
 }
