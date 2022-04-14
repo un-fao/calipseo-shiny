aca3b7606417ad9b19bd0aa303b898eb2e016ca4
Ojok Brian Andrew <46162824+brianandy@users.noreply.github.com>
2022-04-14 18:09:15 +0300

    logo handling improved

--- shiny/shiny-calipseo/ui.R
+++ shiny/shiny-calipseo/ui.R
@@ -1,16 +1,8 @@
-# Define UI for application that draws a histogram
+# Define UI for application
 #==========================================================================================
 ui <- shiny::tagList(
   dashboardPage(
-    dashboardHeader(
-      title = tags$div(
-        tags$a(
-          href=appConfig$country_profile$website,
-          HTML(createBase64Image(src = appConfig$country_profile$logo, width = '100', height = '50', alt = 'Logo'))
-        ),
-        tags$span("Calipseo Dashboard", style = "font-size:80%;")
-      )
-    ),
+    dashboardHeader(title = dbHeader),
     dashboardSidebar(
       sidebarMenuFromModules(appConfig)
     ),
