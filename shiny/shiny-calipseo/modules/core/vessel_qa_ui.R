d8ade468b5b808d5346c04f4bc65422c02f8122f
Ojok Brian Andrew <46162824+brianandy@users.noreply.github.com>
2022-04-20 20:31:10 +0300

    tab on vessels count with or without fishing activity added

--- shiny/shiny-calipseo/modules/core/vessel_qa_ui.R
+++ shiny/shiny-calipseo/modules/core/vessel_qa_ui.R
@@ -17,7 +17,8 @@ vessel_qa_ui <- function(id){
                 shinyWidgets::verticalTabPanel(i18n("VERTICALTABPANEL_VESSEL_QA_VOP_STATUS"),box_height='70px',box(width = 12, height = 480, title = i18n("VESSEL_QA_TITLE_VOP_STATUS"),DT::dataTableOutput(ns("vessel_operational_status")))),
                 shinyWidgets::verticalTabPanel(i18n("VERTICALTABPANEL_VESSEL_QA_PORTS"),box_height='70px' ,box(width = 12, height = 480, title = i18n("VESSEL_QA_TITLE_PORTS"), DT::dataTableOutput(ns("vessel_ports"))),sliderWidth =25),
                 shinyWidgets::verticalTabPanel(i18n("VERTICALTABPANEL_VESSEL_QA_CHARACTERISTICS"),box_height='70px',box(width = 12, height = 480, title = i18n("VESSEL_QA_TITLE_CHARACTERISTICS"),DT::dataTableOutput(ns("vessel_characteristics")))),
-                shinyWidgets::verticalTabPanel(i18n("VERTICALTABPANEL_VESSEL_QA_LICENSE"),box_height='70px',box(width = 12, height = 480, title = i18n("VESSEL_QA_TITLE_LICENSE"),DT::dataTableOutput(ns("vessel_license"))))))
+                shinyWidgets::verticalTabPanel(i18n("VERTICALTABPANEL_VESSEL_QA_LICENSE"),box_height='70px',box(width = 12, height = 480, title = i18n("VESSEL_QA_TITLE_LICENSE"),DT::dataTableOutput(ns("vessel_license")))),
+                shinyWidgets::verticalTabPanel(i18n("VERTICALTABPANEL_VESSEL_QA_ACTIVITY"),box_height='70px',box(width = 12, height = 480, title = i18n("VESSEL_QA_TITLE_ACTIVITY"),DT::dataTableOutput(ns("vessel_activity"))))))
   )
   
 }
