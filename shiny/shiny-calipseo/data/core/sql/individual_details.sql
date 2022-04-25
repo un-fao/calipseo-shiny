f8e01370bb4c22cc20215a4f443a5e9ae9215284
Ojok Brian Andrew <46162824+brianandy@users.noreply.github.com>
2022-04-25 16:16:03 +0300

    join fixed

--- shiny/shiny-calipseo/data/core/sql/individual_details.sql
+++ shiny/shiny-calipseo/data/core/sql/individual_details.sql
@@ -1,6 +1,6 @@
 SELECT salu.I18n_DEFAULT as Salutation, ind.FIRST_NAME, ind.MIDDLE_NAME, ind.SUFFIX_NAME, 
 ent.NAME, gend.I18n_DEFAULT AS Gender FROM reg_entities AS ent 
-LEFT JOIN reg_entity_individuals AS ind ON ent.ID = ind.ID 
+LEFT JOIN reg_entity_individuals AS ind ON ent.ID = ind.REG_ENTITY_ID 
 LEFT JOIN cl_app_genders AS gend ON ind.CL_APP_GENDER_ID = gend.ID 
 LEFT JOIN cl_app_salutations AS salu ON ind.CL_APP_SALUTATION_ID = salu.ID 
 /*left join reg_vessels as v on ind.REG_ENTITY_ID = v.REG_ENTITY_OWNER_ID*/
