ab2ed17b5c46d5606f3dce8b8c8f76c45b7f82f4
Ojok Brian Andrew <46162824+brianandy@users.noreply.github.com>
2022-04-28 23:51:42 +0300

    bug fixed

--- shiny/shiny-calipseo/data/core/sql/vessel_license_permits.sql
+++ shiny/shiny-calipseo/data/core/sql/vessel_license_permits.sql
@@ -2,9 +2,8 @@ SELECT
 vlp.PERMIT_NUMBER, vlp.APPLICATION_DATE, vlp.PERMIT_DATE,
 vlp.DATE_FROM AS Valid_from_date, vlp.DATE_TO AS Valid_to_date,
 rg.I18n_DEFAULT AS Gears, v.REGISTRATION_NUMBER 
-FROM reg_vessel_gears AS vg 
-LEFT JOIN reg_vessels AS v ON vg.REG_VESSEL_ID = v.ID 
+FROM reg_vessels AS v 
+LEFT JOIN reg_vessel_license_permits AS vlp ON v.ID = vlp.REG_VESSEL_ID  
+LEFT JOIN reg_vessel_gears AS vg ON vlp.REG_VESSEL_ID = vg.REG_VESSEL_ID 
 LEFT JOIN cl_ref_gears AS rg ON vg.CL_REF_GEAR_ID = rg.ID 
-LEFT JOIN reg_vessel_license_permits AS vlp ON vg.REG_VESSEL_ID = vlp.REG_VESSEL_ID 
-
 
