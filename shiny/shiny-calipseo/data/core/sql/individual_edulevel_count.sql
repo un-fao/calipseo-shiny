c8a735c2a27ce1a36fcbe63fa495c713ea8a92bb
eblondelfao <emmanuel.blondel@fao.org>
2022-08-16 16:35:41 +0200

    fix left join #124

--- shiny/shiny-calipseo/data/core/sql/individual_edulevel_count.sql
+++ shiny/shiny-calipseo/data/core/sql/individual_edulevel_count.sql
@@ -1,5 +1,5 @@
 SELECT edulevel.CODE, edulevel.I18n_DEFAULT as NAME, count(*) as COUNT FROM reg_entity_individual_education AS edu 
-LEFT JOIN reg_entities AS ent ON edu.REG_ENTITY_INDIVIDUAL_ID = ent.ID 
+LEFT JOIN reg_entities AS ent ON ind.REG_ENTITY_ID = ent.ID 
 LEFT JOIN cl_app_education_levels AS edulevel ON edu.CL_APP_EDUCATION_LEVEL_ID = edulevel.ID 
 LEFT JOIN reg_entity_individuals AS ind ON edu.ID = ind.ID 
 LEFT JOIN cl_app_genders as gend ON CL_APP_GENDER_ID = gend.ID 
