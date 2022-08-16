70291cff895b6409e14fc1c7a0d8cedbc1d6c4de
Ojok Brian Andrew <46162824+brianandy@users.noreply.github.com>
2022-08-16 12:59:32 +0300

    minor change

--- shiny/shiny-calipseo/data/core/sql/individual_details.sql
+++ shiny/shiny-calipseo/data/core/sql/individual_details.sql
@@ -3,4 +3,4 @@ ent.NAME, gend.I18n_DEFAULT AS Gender, ind.DATE_OF_BIRTH AS DOB, ind.REG_ENTITY_
 LEFT JOIN reg_entity_individuals AS ind ON ent.ID = ind.REG_ENTITY_ID 
 LEFT JOIN cl_app_genders AS gend ON ind.CL_APP_GENDER_ID = gend.ID 
 LEFT JOIN cl_app_salutations AS salu ON ind.CL_APP_SALUTATION_ID = salu.ID 
-WHERE ent.ENABLED = 1
+WHERE ent.ENABLED = 1 AND ind.ID IS NOT NULL
