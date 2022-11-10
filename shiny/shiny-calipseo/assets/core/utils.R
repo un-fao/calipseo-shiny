52063b397fe2e14ae7add96b10cdebf519c301bc
Bennici <alexandre-ben@hotmail.fr>
2022-11-10 14:42:32 +0100

    #142 implement generic method and task algo wrapper

--- shiny/shiny-calipseo/assets/core/utils.R
+++ shiny/shiny-calipseo/assets/core/utils.R
@@ -14,3 +14,62 @@ round2 <- function(x, n=0) {scale<-10^n; trunc(x*scale+sign(x)*0.5)/scale}
 NumberOfDays <- function(date) {
   return(as.numeric(format(as.Date(paste0(format(date,format="%Y"),formatC(ifelse(format(date,format="%m")=="12",0,as.numeric(format(date,format="%m")))+1,width=2,format="d",flag="0"),"01"),"%Y%m%d")-1,format="%d")))
 }
+
+#Convert Quarter to date
+qtodate<- function(date,period="start"){
+  
+  date<-unlist(strsplit(date,"-") )
+  
+  Y<-date[1]
+  Q<-date[2]
+  
+  Q<-switch (Q,
+             Q1=c('01-01','03-31'),
+             Q2=c('04-01','06-30'),
+             Q3=c('07-01','09-30'),
+             Q4=c('10-01','12-31')
+  )
+  
+  date<-paste0(Y,"-",Q)
+  
+  if(period=="start")return(date[1])
+  if(period=="end")return(date[2])
+  if(period=="start+end")return(paste0(date[1],"/",date[2]))
+  
+}
+
+#Convert Year-Month to date
+ymtodate<- function(date,period="start"){
+  start_date<-paste0(date,"-01")
+  end_date<-as.character(ceiling_date(ymd(start_date),unit= "month") -1)
+  if(period=="start")return(start_date)
+  if(period=="end")return(end_date)
+  if(period=="start+end")return(paste0(start_date,"/",end_date))
+  
+}
+
+#Convert Year to date
+ytodate<- function(date,period="start"){
+  Y<-unlist(strsplit(date[i],"-"))[1]
+  start_date<-paste0(Y,"-01-01")
+  end_date<-paste0(Y,"-12-31")
+  if(period=="start")return(start_date)
+  if(period=="end")return(end_date)
+  if(period=="start+end")return(paste0(start_date,"/",end_date))
+}
+
+#Manage Date formating
+dateformating<- function(date,period="start"){
+  dates<-c()
+  for(i in c(1:length(date))){
+    if(startsWith(unlist(strsplit(date[i],"-"))[2],"Q")){
+      x<-qtodate(date[i],period=period)
+    }else if(unlist(strsplit(date[i],"-"))[2]=="NA"){
+      x<-ytodate(date[i],period=period)
+    }else{
+      x<-ymtodate(date[i],period=period)
+    }
+    dates<-c(dates,x)
+  }
+  return(dates)
+}
--- shiny/shiny-calipseo/modules/country/SUR/logbooks_export_server.R
+++ shiny/shiny-calipseo/modules/country/SUR/logbooks_export_server.R
@@ -64,20 +64,47 @@ logbooks_export_server <- function(id, pool) {
          sprintf('WECAFC_SUR_%s_%s_%s_%s.csv',input$year,input$task,input$format,input$aggregate)
        },
        content = function(file) {
-         data<-accessLogBooksWecafc(pool,year=input$year)
-         data<-data%>%
-          rowwise()%>%
-            mutate(period=ifelse(input$aggregate=="month",sprintf('%02d',as.integer(period)),ifelse(as.integer(period)%in%c(1:3),"Q1",
-                                                                                                   ifelse(as.integer(period)%in%c(4:6),"Q2",
-                                                                                                          ifelse(as.integer(period)%in%c(7:9),"Q3","Q4")))))%>%
-           group_by(flagstate,year,period,geographical_identifier,species,catch_unit)%>%
-           summarise(catch_retained=sum(ifelse(catch_unit=="Kilogram"&!is.null(catch_retained),catch_retained/1000,catch_retained),na.rm=T),
-                     catch_discarded=sum(ifelse(catch_unit=="Kilogram"&!is.null(catch_discarded),catch_discarded/1000,catch_discarded),na.rm=T),
-                     catch_nominal=sum(ifelse(catch_unit=="Kilogram"&!is.null(catch_nominal),catch_nominal/1000,catch_nominal),na.rm=T))%>%
-           ungroup()%>%
-           mutate(catch_unit=ifelse(catch_unit=="Kilogram","t",catch_unit))%>%
-           select(flagstate,year,period,geographical_identifier,species,catch_retained,catch_discarded,catch_nominal,catch_unit)
-         print(as.data.frame(data))
+         switch(input$task,
+          "task_1.2"={
+             data<-accessLogBooksWecafc(pool,year=input$year)
+             data<-data%>%
+              rowwise()%>%
+                mutate(period=ifelse(input$aggregate=="month",sprintf('%02d',as.integer(period)),ifelse(as.integer(period)%in%c(1:3),"Q1",
+                                                                                                       ifelse(as.integer(period)%in%c(4:6),"Q2",
+                                                                                                              ifelse(as.integer(period)%in%c(7:9),"Q3","Q4")))))%>%
+               group_by(flagstate,year,period,geographic_identifier,species,catch_unit)%>%
+               summarise(catch_retained=sum(ifelse(catch_unit=="Kilogram"&!is.null(catch_retained),catch_retained/1000,catch_retained),na.rm=T),
+                         catch_discarded=sum(ifelse(catch_unit=="Kilogram"&!is.null(catch_discarded),catch_discarded/1000,catch_discarded),na.rm=T),
+                         catch_nominal=sum(ifelse(catch_unit=="Kilogram"&!is.null(catch_nominal),catch_nominal/1000,catch_nominal),na.rm=T))%>%
+               ungroup()%>%
+               mutate(catch_unit=ifelse(catch_unit=="Kilogram","t",catch_unit))%>%
+               select(flagstate,year,period,geographic_identifier,species,catch_retained,catch_discarded,catch_nominal,catch_unit)
+             
+             if(input$format=="generic"){
+               data<-data%>%
+                 pivot_longer(c(catch_retained,catch_discarded,catch_nominal),names_to="measurement_type",values_to="measurement_value")%>%
+                 mutate(measurement_type=gsub("^.*?_","",measurement_type))%>%
+                 rename(measurement_unit=catch_unit)%>%
+               mutate(measurement="catch")%>%
+               mutate(date=paste0(year,"-",period))%>%
+               mutate(time_start=dateformating(date,"start"),
+                      time_end=dateformating(date,"end"),
+                      time=paste0(time_start,"/",time_end))%>%
+               select(c(flagstate,
+                        time,
+                        time_start,
+                        time_end, 
+                        geographic_identifier,
+                        species,
+                        measurement,
+                        measurement_type,
+                        measurement_value,
+                        measurement_unit))
+             }
+         }
+         )
+         
+         
          write.csv(data, file,row.names = F)
        }
      )
