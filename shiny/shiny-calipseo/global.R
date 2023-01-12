42b00135feeb7630470aee495c1a13af2ff031ce
eblondelfao <emmanuel.blondel@fao.org>
2023-01-12 16:12:17 +0100

    #150 missing code

--- shiny/shiny-calipseo/global.R
+++ shiny/shiny-calipseo/global.R
@@ -35,7 +35,7 @@ if(is.null(appConfig$language)) appConfig$language <- "en"
 default_store_dir <- "/srv/shiny-server/shiny-calipseo-store"
 #shiny-storage in dev (assume Windows OS for now)
 if(Sys.info()[["sysname"]] == "Windows") default_store_dir <- "out"
-if(is.null(appConfig$store)) appConfig$store
+if(is.null(appConfig$store)) appConfig$store <- default_store_dir
 if(!dir.exists(appConfig$store)) dir.create(appConfig$store)
 
 #DB connections
