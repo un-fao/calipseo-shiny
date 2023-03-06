5f26775919c6d9e3f1d1862a1626a2191111d468
Bennici <alexandre-ben@hotmail.fr>
2023-03-06 16:30:25 +0100

    #159 create GFCM Task II.2 aggregation

---
+++ shiny/shiny-calipseo/assets/country/LBN/compute_gfcm_task_II_2.R
@@ -0,0 +1,21 @@
+###compute_gfcm_task_II_2
+compute_gfcm_task_II_2<-function(ref_species,data){
+  
+  data<-data%>%
+    group_by(EST_YEAR,EST_FS,EST_SPC)%>%
+    summarise(TOTAL_LANDING= sum(EST_LND_CATCH,na.rm=T),
+              TOTAL_DISCARDS = 0,
+              TOTAL_CATCH = sum(EST_LND_CATCH,na.rm=T)
+              )%>%
+    ungroup()%>%
+    mutate(COMMENT="",
+           GSA="27",
+           EST_SPC=as.character(EST_SPC))%>%
+    left_join(ref_species%>%select(ID,ASFIS_CODE)%>%mutate(ID=as.character(ID)),by=c('EST_SPC'='ID'))%>%
+    rename(FLEET_SEGMENT=EST_FS,
+           SPECIES=ASFIS_CODE)%>%
+    select(GSA,FLEET_SEGMENT,SPECIES,TOTAL_LANDING,TOTAL_DISCARDS,TOTAL_CATCH,COMMENT)
+  
+  return(data)
+  
+}
