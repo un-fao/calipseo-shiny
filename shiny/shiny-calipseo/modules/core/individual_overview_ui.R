4e2b77612e1df83e4425dde2c959360a0c537deb
Bennici <alexandre-ben@hotmail.fr>
2023-07-24 16:41:24 +0200

    #174 add indicators and clean module

--- shiny/shiny-calipseo/modules/core/individual_overview_ui.R
+++ shiny/shiny-calipseo/modules/core/individual_overview_ui.R
@@ -6,6 +6,14 @@ individual_overview_ui <- function(id){
   tabItem(tabName = "individual_overview",
           div(class = 'row',style = "margin:12px;",htmlOutput(ns("individual_overview_info"))),
           div(class = 'row',
+              div(class = 'col-md-6',
+                box(width = 12,title = i18n("INDIVIDUAL_OVERVIEW_INDICATORS_BOX_LABEL"),uiOutput(ns("indicators")))
+                ),
+              div(class = 'col-md-6',
+                box(width = 12,title = i18n("INDIVIDUAL_OVERVIEW_MAP_BOX_LABEL"),p("Map place holder"))
+                )
+              ),
+          div(class = 'row',
               div(class = 'col-md-6',sunburst_chart_ui(ns("sb"),title=i18n("INDIVIDUAL_OVERVIEW_SUNBURST_BOX_LABEL"),sliderWidth =25)),
               div(class = 'col-md-6',pyramid_chart_ui(ns("py"),title=i18n("INDIVIDUAL_OVERVIEW_PYRAMID_BOX_LABEL"),sliderWidth =25))
           ),
