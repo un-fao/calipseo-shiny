3433366392d54d44d962899dfe0703b5acf8e40b
Bennici <alexandre-ben@hotmail.fr>
2023-08-19 22:35:14 +0200

    #177 test

--- shiny/shiny-calipseo/modules/core/individual_list_server.R
+++ shiny/shiny-calipseo/modules/core/individual_list_server.R
@@ -12,13 +12,74 @@ individual_list_server <- function(id, pool) {
     
     
     ind <- accessIndividualDetails(pool)
-    
-    
+  
     
     INFO("individual-list server: Fetching individual list data with rows '%s'", nrow(ind))
     
     
-    
+    # ind_info <- accessIndividualInfo(pool)
+    # ind_info <- ind_info[names(ind_info)%in%c("ID","Salutations","First_name","Middle_name","Suffix_name","Gender","FisherID","Site")]
+    # 
+    # print(ind_info)
+    # 
+    # country_params<-accessCountryParam(pool)
+    # 
+    # is_fisher_query<-subset(country_params,CODE=="ISFISHER")$TEXT
+    # if(length(is_fisher_query)>0){
+    #   is_fisher_table<-suppressWarnings(dbGetQuery(pool, is_fisher_query))
+    #   names(is_fisher_table)<-c("ID","Type")
+    #   is_fisher_table$Type[is_fisher_table$Type==0]<-"Non Fisher"
+    #   is_fisher_table$Type[is_fisher_table$Type==1]<-"Fisher"
+    #   ind_info<-merge(ind_info,is_fisher_table)
+    # }else{
+    #   ind_info$Type<-NA
+    # }
+    # 
+    # is_fisher_active_query<-subset(country_params,CODE=="ISFISHERACTIVE")$TEXT
+    # if(length(is_fisher_active_query)>0){
+    #   is_fisher_active_table<-suppressWarnings(dbGetQuery(pool, is_fisher_active_query))
+    #   names(is_fisher_active_table)<-c("ID","Active")
+    #   is_fisher_active_table$Active[is_fisher_active_table$Active==0]<-"Fisher Non Active"
+    #   is_fisher_active_table$Active[is_fisher_active_table$Active==1]<-"Fisher Active"
+    #   is_fisher_active_table$Active[is.na(is_fisher_active_table$Active)]<-"Non Fisher"
+    #   ind_info<-merge(ind_info,is_fisher_active_table)
+    # }else{
+    #   ind_info$Active<-NA
+    # }
+    # 
+    # ind_info<-unique(ind[!names(ind_info)=="ID"])
+    # 
+    # 
+    # 
+    # INFO("individual-list server: Fetching individual list data with rows '%s'", nrow(ind_info))
+    # 
+    # output$individual_list <- renderDT(
+    #   ind_info,
+    #   server = FALSE,
+    #   escape = FALSE,
+    #   rownames = FALSE,
+    #   extensions = c("Buttons"),
+    #   filter = list(position = 'top', clear = FALSE),
+    #   
+    #   options = list(
+    #     autoWidth = FALSE,
+    #     dom = 'Bfrtip',
+    #     deferRender = TRUE,
+    #     scroll = FALSE,
+    #     buttons = list(
+    #       list(extend = 'copy'),
+    #       list(extend = 'csv', filename = i18n("INDIVIDUALS") , title = NULL, header = TRUE),
+    #       list(extend = 'excel', filename =  i18n("INDIVIDUALS"), title = NULL, header = TRUE),
+    #       list(extend = "pdf", title = i18n("INDIVIDUAL_LIST_TITLE"), header = TRUE, orientation = "landscape")
+    #     ),
+    #     exportOptions = list(
+    #       modifiers = list(page = "all", selected = TRUE)
+    #     ),
+    #     language = list(url = i18n("TABLE_LANGUAGE")),
+    #     
+    #     pageLength = 10
+    #   )
+    # ) 
     
     
     
