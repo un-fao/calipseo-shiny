2d39bb1e41ccca0f7d5ba577f23167a545c12f14
Bennici <alexandre-ben@hotmail.fr>
2023-10-10 08:43:57 +0200

    #178 refactor individual_list

--- shiny/shiny-calipseo/modules/core/individual_list_server.R
+++ shiny/shiny-calipseo/modules/core/individual_list_server.R
@@ -17,249 +17,96 @@ individual_list_server <- function(id, pool) {
     INFO("individual-list server: Fetching individual list data with rows '%s'", nrow(ind))
     
     
-    # ind_info <- accessIndividualInfo(pool)
-    # ind_info <- ind_info[names(ind_info)%in%c("ID","Salutations","First_name","Middle_name","Suffix_name","Gender","FisherID","Site")]
-    # 
-    # print(ind_info)
-    # 
-    # country_params<-accessCountryParam(pool)
-    # 
-    # is_fisher_query<-subset(country_params,CODE=="ISFISHER")$TEXT
-    # if(length(is_fisher_query)>0){
-    #   is_fisher_table<-suppressWarnings(dbGetQuery(pool, is_fisher_query))
-    #   names(is_fisher_table)<-c("ID","Type")
-    #   is_fisher_table$Type[is_fisher_table$Type==0]<-"Non Fisher"
-    #   is_fisher_table$Type[is_fisher_table$Type==1]<-"Fisher"
-    #   ind_info<-merge(ind_info,is_fisher_table)
-    # }else{
-    #   ind_info$Type<-NA
-    # }
-    # 
-    # is_fisher_active_query<-subset(country_params,CODE=="ISFISHERACTIVE")$TEXT
-    # if(length(is_fisher_active_query)>0){
-    #   is_fisher_active_table<-suppressWarnings(dbGetQuery(pool, is_fisher_active_query))
-    #   names(is_fisher_active_table)<-c("ID","Active")
-    #   is_fisher_active_table$Active[is_fisher_active_table$Active==0]<-"Fisher Non Active"
-    #   is_fisher_active_table$Active[is_fisher_active_table$Active==1]<-"Fisher Active"
-    #   is_fisher_active_table$Active[is.na(is_fisher_active_table$Active)]<-"Non Fisher"
-    #   ind_info<-merge(ind_info,is_fisher_active_table)
-    # }else{
-    #   ind_info$Active<-NA
-    # }
-    # 
-    # ind_info<-unique(ind[!names(ind_info)=="ID"])
-    # 
-    # 
-    # 
-    # INFO("individual-list server: Fetching individual list data with rows '%s'", nrow(ind_info))
-    # 
-    # output$individual_list <- renderDT(
-    #   ind_info,
-    #   server = FALSE,
-    #   escape = FALSE,
-    #   rownames = FALSE,
-    #   extensions = c("Buttons"),
-    #   filter = list(position = 'top', clear = FALSE),
-    #   
-    #   options = list(
-    #     autoWidth = FALSE,
-    #     dom = 'Bfrtip',
-    #     deferRender = TRUE,
-    #     scroll = FALSE,
-    #     buttons = list(
-    #       list(extend = 'copy'),
-    #       list(extend = 'csv', filename = i18n("INDIVIDUALS") , title = NULL, header = TRUE),
-    #       list(extend = 'excel', filename =  i18n("INDIVIDUALS"), title = NULL, header = TRUE),
-    #       list(extend = "pdf", title = i18n("INDIVIDUAL_LIST_TITLE"), header = TRUE, orientation = "landscape")
-    #     ),
-    #     exportOptions = list(
-    #       modifiers = list(page = "all", selected = TRUE)
-    #     ),
-    #     language = list(url = i18n("TABLE_LANGUAGE")),
-    #     
-    #     pageLength = 10
-    #   )
-    # ) 
-    
-    
-    
-    observe({
+     ind_info <- accessIndividualInfo(pool)
+     ind_info <- ind_info[names(ind_info)%in%c("ID","Salutations","First_name","Middle_name","Suffix_name","Last_name","Gender","FisherID","Site")]
+     
+     country_params<-accessCountryParam(pool)
+     
+     is_fisher_query<-subset(country_params,CODE=="ISFISHER")$TEXT
+     if(length(is_fisher_query)>0){
+       is_fisher_table<-suppressWarnings(dbGetQuery(pool, is_fisher_query))
+       names(is_fisher_table)<-c("ID","Type")
+       is_fisher_table$Type[is_fisher_table$Type==0]<-"Non Fisher"
+       is_fisher_table$Type[is_fisher_table$Type==1]<-"Fisher"
+       ind_info<-merge(ind_info,is_fisher_table)
+     }else{
+       ind_info$Type<-NA
+     }
+     
+     is_fisher_active_query<-subset(country_params,CODE=="ISFISHERACTIVE")$TEXT
+     if(length(is_fisher_active_query)>0){
+       is_fisher_active_table<-suppressWarnings(dbGetQuery(pool, is_fisher_active_query))
+       names(is_fisher_active_table)<-c("ID","Status")
+       is_fisher_active_table$Status[is_fisher_active_table$Status==0]<-"Inactive"
+       is_fisher_active_table$Status[is_fisher_active_table$Status==1]<-"Active"
+       is_fisher_active_table$Status[is.na(is_fisher_active_table$Status)]<-""
+       ind_info<-merge(ind_info,is_fisher_active_table)
+     }else{
+       ind_info$Active<-NA
+     }
+     
+     ind_info<-unique(subset(ind_info,select=c(Type,Status,FisherID,Salutations,First_name,Middle_name,Suffix_name,Last_name,Gender,Site,ID)))
+     ind_info<-ind_info[!sapply(ind_info, function(x) all(x == ""|is.na(x)))]
+     
+     # ind_info$Details <- sapply(ind_info$ID, function(x){
+     #  ind_outhtml <- sprintf("<a href=\"./?page=individual-info&individualNumber=%s\" style=\"font-weight:bold;\">Details</a>", x)
+     #  return(ind_outhtml)
+     # })
+              
+      ind_info<-ind_info[names(ind_info)!="ID"]
       
-      if(appConfig[["country_profile"]]$data$CODE=="DM"){
-        
-        observeEvent(input$fisher_chck_list,{if(isTRUE(input$fisher_chck_list))updateCheckboxInput(session,"nonfisher_chck_list",value = FALSE)})
-        
-        
-        
-        
-        
-        
-        observeEvent({
-          
-          input$nonfisher_chck_list
-          input$filter_fisher_category_list
-          
-        },
-        {
-          if(isFALSE(input$nonfisher_chck_list)){
-            updateCheckboxInput(session,"fisher_chck_list",value = TRUE)
-            category <- i18n("INDIVIDUAL_BREAKDOWN_LABEL_FISHER")
-          }else {
-            updateCheckboxInput(session,"fisher_chck_list",value = FALSE)
-            category <- i18n("INDIVIDUAL_BREAKDOWN_LABEL_NONFISHER")
-            
-          }
-          
-          
-          if(category==i18n("INDIVIDUAL_BREAKDOWN_LABEL_FISHER")){
-            
-            
-            ind <- filter_category(data = ind,input = input$filter_fisher_category_list,column_indexes = c(10:13))
-            
-            
-            INFO("individual-list server: Country choosen is '%s'",appConfig[["country_profile"]]$data$NAME)
-            
-            INFO("individual-list server: Indiviadual list filtered by individual roles '%s'", input$filter_fisher_category_list)
-            
-            ind$Details <- sapply(ind$IndividualNumber, function(x){
-              ind_outhtml <- sprintf("<a href=\"./?page=individual-info&individualNumber=%s\" style=\"font-weight:bold;\">Details</a>", x)
-              return(ind_outhtml)
-            })
-            
-            
-            
-            ind <- ind[,c("Salutation","FIRST_NAME","MIDDLE_NAME","SUFFIX_NAME","NAME","Gender","Details")]
-            INFO("individual-list server: Applying the I18n_terms to the individual list columns")
-            names(ind) <- c(i18n("INDIVIDUAL_LIST_TABLE_COLNAME_1"),i18n("INDIVIDUAL_LIST_TABLE_COLNAME_2"),
-                            i18n("INDIVIDUAL_LIST_TABLE_COLNAME_3"),i18n("INDIVIDUAL_LIST_TABLE_COLNAME_4"),
-                            i18n("INDIVIDUAL_LIST_TABLE_COLNAME_5"),i18n("INDIVIDUAL_LIST_TABLE_COLNAME_6"),"")
-            
-            
-            output$individual_list <- renderDT(
-              ind,
-              server = FALSE,
-              escape = FALSE,
-              rownames = FALSE,
-              extensions = c("Buttons"),
-              filter = list(position = 'top', clear = FALSE),
-              
-              options = list(
-                autoWidth = FALSE,
-                dom = 'Bfrtip',
-                deferRender = TRUE,
-                scroll = FALSE,
-                buttons = list(
-                  list(extend = 'copy'),
-                  list(extend = 'csv', filename = i18n("INDIVIDUALS") , title = NULL, header = TRUE),
-                  list(extend = 'excel', filename =  i18n("INDIVIDUALS"), title = NULL, header = TRUE),
-                  list(extend = "pdf", title = i18n("INDIVIDUAL_LIST_TITLE"), header = TRUE, orientation = "landscape")
-                ),
-                exportOptions = list(
-                  modifiers = list(page = "all", selected = TRUE)
-                ),
-                language = list(url = i18n("TABLE_LANGUAGE")),
-                
-                pageLength = 10
-              )
-            )
-            
-            
-          }else{
-            
-            ind <- ind %>% dplyr::filter(Category%in%'nonfisher')
-            
-            ind$Details <- sapply(ind$IndividualNumber, function(x){
-              ind_outhtml <- sprintf("<a href=\"./?page=individual-info&individualNumber=%s\" style=\"font-weight:bold;\">Details</a>", x)
-              return(ind_outhtml)
-            })
-            
-            
-            
-            ind <- ind[,c("Salutation","FIRST_NAME","MIDDLE_NAME","SUFFIX_NAME","NAME","Gender","Details")]
-            INFO("individual-list server: Applying the I18n_terms to the individual list columns")
-            names(ind) <- c(i18n("INDIVIDUAL_LIST_TABLE_COLNAME_1"),i18n("INDIVIDUAL_LIST_TABLE_COLNAME_2"),
-                            i18n("INDIVIDUAL_LIST_TABLE_COLNAME_3"),i18n("INDIVIDUAL_LIST_TABLE_COLNAME_4"),
-                            i18n("INDIVIDUAL_LIST_TABLE_COLNAME_5"),i18n("INDIVIDUAL_LIST_TABLE_COLNAME_6"),"")
-            
-            
-            output$individual_list <- renderDT(
-              ind,
-              server = FALSE,
-              escape = FALSE,
-              rownames = FALSE,
-              extensions = c("Buttons"),
-              filter = list(position = 'top', clear = FALSE),
-              
-              options = list(
-                autoWidth = FALSE,
-                dom = 'Bfrtip',
-                deferRender = TRUE,
-                scroll = FALSE,
-                buttons = list(
-                  list(extend = 'copy'),
-                  list(extend = 'csv', filename = i18n("INDIVIDUALS") , title = NULL, header = TRUE),
-                  list(extend = 'excel', filename =  i18n("INDIVIDUALS"), title = NULL, header = TRUE),
-                  list(extend = "pdf", title = i18n("INDIVIDUAL_LIST_TITLE"), header = TRUE, orientation = "landscape")
-                ),
-                exportOptions = list(
-                  modifiers = list(page = "all", selected = TRUE)
-                ),
-                language = list(url = i18n("TABLE_LANGUAGE")),
-                
-                pageLength = 10
-              )
-            ) 
-            
-            
-          }
-          
-        })
-        
-        
-      }else{
-        
-        
-        INFO("individual-list server: Country choosen is '%s'",appConfig[["country_profile"]]$data$NAME)
-        
-        ind <- ind[,c("Salutation","FIRST_NAME","MIDDLE_NAME","SUFFIX_NAME","NAME","Gender")]
-        
-        INFO("individual-list server: Applying the I18n_terms to the individual list columns")
-        names(ind) <- c(i18n("INDIVIDUAL_LIST_TABLE_COLNAME_1"),i18n("INDIVIDUAL_LIST_TABLE_COLNAME_2"),
-                        i18n("INDIVIDUAL_LIST_TABLE_COLNAME_3"),i18n("INDIVIDUAL_LIST_TABLE_COLNAME_4"),
-                        i18n("INDIVIDUAL_LIST_TABLE_COLNAME_5"),i18n("INDIVIDUAL_LIST_TABLE_COLNAME_6"))
+      for(i in 1:length(names(ind_info))){
+        names(ind_info)[i] <- i18n(sprintf("INDIVIDUAL_LIST_TABLE_COLNAME_%s",toupper(names(ind_info)[i])))
       }
+     
       
-      INFO("indiviadual-list server: Rendering the individual list data to the table") 
+      js <- js_select2_filter_provider(ns("individual_list"))
+     
+      ind_info$Status<-ifelse(ind_info$Status=="Active",paste0(icon("anchor-circle-check",style = 'color:green'),span(" Active",style='color:green;')),
+                              ifelse(ind_info$Status=="Inactive",paste0(icon("anchor-circle-exclamation",style = 'color:orange'),span(" Inactive",style='color:orange;')),
+                                     ""))
+      ind_info$Gender<-ifelse(ind_info$Gender=="Male",paste0(icon("mars"),span(" Male")),
+                              ifelse(ind_info$Gender=="Female",paste0(icon("venus"),span(" Female")),
+                                     ind$Gender))
+
       
-      output$individual_list <- renderDT(
-        ind,
-        server = FALSE,
-        escape = FALSE,
-        rownames = FALSE,
-        extensions = c("Buttons"),
-        filter = list(position = 'top', clear = FALSE),
-        
-        options = list(
-          autoWidth = FALSE,
-          dom = 'Bfrtip',
-          deferRender = TRUE,
-          scroll = FALSE,
-          buttons = list(
-            list(extend = 'copy'),
-            list(extend = 'csv', filename = i18n("INDIVIDUALS") , title = NULL, header = TRUE),
-            list(extend = 'excel', filename =  i18n("INDIVIDUALS"), title = NULL, header = TRUE),
-            list(extend = "pdf", title = i18n("INDIVIDUAL_LIST_TITLE"), header = TRUE, orientation = "landscape")
-          ),
-          exportOptions = list(
-            modifiers = list(page = "all", selected = TRUE)
-          ),
-          language = list(url = i18n("TABLE_LANGUAGE")),
-          
-          pageLength = 10
-        )
+     
+     print("START TEST PRINT")
+     print(ind_info)
+     print("END TEST PRINT")
+
+
+
+    INFO("individual-list server: Fetching individual list data with rows '%s'", nrow(ind_info))
+
+    output$individual_list <- renderDT(
+      ind_info,
+      container = initDTContainer(ind_info),
+      server = FALSE,
+      escape = FALSE,
+      rownames = FALSE,
+      extensions = c("Buttons"),
+      filter = list(position = 'top', clear = FALSE),
+
+      options = list(
+        autoWidth = FALSE,
+        dom = 'Bfrtip',
+        deferRender = TRUE,
+        scroll = FALSE,
+        buttons = list(
+          list(extend = 'copy'),
+          list(extend = 'csv', filename = i18n("INDIVIDUALS") , title = NULL, header = TRUE),
+          list(extend = 'excel', filename =  i18n("INDIVIDUALS"), title = NULL, header = TRUE),
+          list(extend = "pdf", title = i18n("INDIVIDUAL_LIST_TITLE"), header = TRUE, orientation = "landscape")
+        ),
+        exportOptions = list(
+          modifiers = list(page = "all", selected = TRUE)
+        ),
+        language = list(url = i18n("TABLE_LANGUAGE")),
+        pageLength = 10,
+        initComplete = js
       )
-      
-    })
+    )
     
   })
   
