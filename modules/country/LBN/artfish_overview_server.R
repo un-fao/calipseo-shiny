ac7c5905c483aa16531d792adb7021b9e444538c
Bennici <alexandre-ben@hotmail.fr>
2024-06-11 09:54:20 +0200

    #214 fix counting

--- modules/country/LBN/artfish_overview_server.R
+++ modules/country/LBN/artfish_overview_server.R
@@ -12,7 +12,8 @@ artfish_overview_server <- function(id, pool){
   ref_species<-accessRefSpecies(pool)
   ref_fishing_units<-accessRefFishingUnits(pool)
   
-  estimate<-artfish_estimates(data_effort=accessEffortData(pool),data_landing=accessLandingData(pool))
+  
+  estimate<-artfish_estimates(con=pool,data_effort=accessEffortData(pool),data_landing=accessLandingData(pool))
   
   output$fishing_unit_selector<-renderUI({
         
@@ -32,10 +33,10 @@ artfish_overview_server <- function(id, pool){
     if(as.integer(input$bg)>0){
       
       selection<-subset(estimate,EST_BGC==input$bg,
-                        select=c(EST_YEAR,EST_MONTH,EST_BGC,EST_SPC,EST_EFF_EFFORT,EST_EFF_NBOATS,EST_LND_CATCH,EST_LND_PRICE,EST_LND_VALUE,EST_LND_CPUE))
+                        select=c(EST_YEAR,EST_MONTH,EST_BGC,EST_BGC_NAME,EST_SPC,EST_SPC_NAME,EST_EFF_EFFORT,EST_EFF_NBOATS,EST_LND_CATCH,EST_LND_PRICE,EST_LND_VALUE,EST_LND_CPUE))
     }else{
       selection<-subset(estimate,
-                        select=c(EST_YEAR,EST_MONTH,EST_BGC,EST_SPC,EST_EFF_EFFORT,EST_EFF_NBOATS,EST_LND_CATCH,EST_LND_PRICE,EST_LND_VALUE,EST_LND_CPUE))
+                        select=c(EST_YEAR,EST_MONTH,EST_BGC,EST_BGC_NAME,EST_SPC,EST_SPC_NAME,EST_EFF_EFFORT,EST_EFF_NBOATS,EST_LND_CATCH,EST_LND_PRICE,EST_LND_VALUE,EST_LND_CPUE))
     }
     data_bg(selection)
     
@@ -46,20 +47,20 @@ artfish_overview_server <- function(id, pool){
     
     data<-data_bg()%>%
      mutate(DATE=as.character(format(as.Date(sprintf("%04d-%02d-01",EST_YEAR,EST_MONTH)),format = "%Y-%m")))%>%
-    left_join(ref_species%>%select(ID,NAME),by=c('EST_SPC'='ID'))%>%
+    #left_join(ref_species%>%select(ID,NAME),by=c('EST_SPC'='ID'))%>%
       ungroup()
     
     data_landing<-data%>%
+                  select(DATE,EST_BGC,EST_BGC_NAME,EST_EFF_EFFORT,EST_EFF_NBOATS)%>%
                   distinct()%>%
-                  select(DATE,EST_BGC,EST_EFF_EFFORT,EST_EFF_NBOATS)%>%
-                  left_join(ref_fishing_units%>%select(ID,NAME),by=c('EST_BGC'='ID'))%>%
+                  #left_join(ref_fishing_units%>%select(ID,NAME),by=c('EST_BGC'='ID'))%>%
                   ungroup()
     
-    artfish_line_chart_server("boats", label=i18n("BOAT_GEAR_LABEL"),df=data_landing, colDate = "DATE",colTarget="NAME",colValue="EST_EFF_NBOATS",ylab=i18n("NUMBER_OF_BOATS_LABEL"),levels=level_choices,stat="sum", rank=TRUE,mode='plot+table',prefered_colnames=c(i18n("TABEL_COLNAME_DATE"),i18n("TABEL_COLNAME_AGG"),i18n("TABEL_COLNAME_BOAT_GEAR")))
-    artfish_line_chart_server("effort", label=i18n("BOAT_GEAR_LABEL"),df=data_landing, colDate = "DATE",colTarget="NAME",colValue="EST_EFF_EFFORT",ylab=i18n("EFFORT_DAYS_LABEL"),levels=level_choices,stat="sum", rank=TRUE,mode='plot+table',prefered_colnames=c(i18n("TABEL_COLNAME_DATE"),i18n("TABEL_COLNAME_AGG"),i18n("TABEL_COLNAME_BOAT_GEAR")))
-    artfish_line_chart_server("catch", label=i18n("SPECIES_LABEL"),df=data, colDate = "DATE",colTarget="NAME",colValue="EST_LND_CATCH",ylab=i18n("CATCH_LABEL"),levels=level_choices,stat="sum", rank=TRUE,mode='plot+table',prefered_colnames=c(i18n("TABEL_COLNAME_DATE"),i18n("TABEL_COLNAME_AGG"),i18n("TABEL_COLNAME_SPECIES")))
-    artfish_line_chart_server("value", label=i18n("SPECIES_LABEL"),df=data, colDate = "DATE",colTarget="NAME",colValue="EST_LND_VALUE",ylab=i18n("VALUE_LABEL"),levels=level_choices,stat="sum", rank=TRUE,mode='plot+table',prefered_colnames=c(i18n("TABEL_COLNAME_DATE"),i18n("TABEL_COLNAME_AGG"),i18n("TABEL_COLNAME_SPECIES")))
-    artfish_line_chart_server("cpue", label=i18n("SPECIES_LABEL"),df=data, colDate = "DATE",colTarget="NAME",colValue="EST_LND_CPUE",ylab=i18n("CPUE_LABEL"),levels=level_choices,stat="mean", rank=TRUE,mode='plot+table',prefered_colnames=c(i18n("TABEL_COLNAME_DATE"),i18n("TABEL_COLNAME_AGG"),i18n("TABEL_COLNAME_SPECIES")))
+    artfish_line_chart_server("boats", label=i18n("BOAT_GEAR_LABEL"),df=data_landing, colDate = "DATE",colTarget="EST_BGC_NAME",colValue="EST_EFF_NBOATS",ylab=i18n("NUMBER_OF_BOATS_LABEL"),levels=level_choices,stat="sum", rank=TRUE,mode='plot+table',prefered_colnames=c(i18n("TABEL_COLNAME_DATE"),i18n("TABEL_COLNAME_AGG"),i18n("TABEL_COLNAME_BOAT_GEAR")))
+    artfish_line_chart_server("effort", label=i18n("BOAT_GEAR_LABEL"),df=data_landing, colDate = "DATE",colTarget="EST_BGC_NAME",colValue="EST_EFF_EFFORT",ylab=i18n("EFFORT_DAYS_LABEL"),levels=level_choices,stat="sum", rank=TRUE,mode='plot+table',prefered_colnames=c(i18n("TABEL_COLNAME_DATE"),i18n("TABEL_COLNAME_AGG"),i18n("TABEL_COLNAME_BOAT_GEAR")))
+    artfish_line_chart_server("catch", label=i18n("SPECIES_LABEL"),df=data, colDate = "DATE",colTarget="EST_SPC_NAME",colValue="EST_LND_CATCH",ylab=i18n("CATCH_LABEL"),levels=level_choices,stat="sum", rank=TRUE,mode='plot+table',prefered_colnames=c(i18n("TABEL_COLNAME_DATE"),i18n("TABEL_COLNAME_AGG"),i18n("TABEL_COLNAME_SPECIES")))
+    artfish_line_chart_server("value", label=i18n("SPECIES_LABEL"),df=data, colDate = "DATE",colTarget="EST_SPC_NAME",colValue="EST_LND_VALUE",ylab=i18n("VALUE_LABEL"),levels=level_choices,stat="sum", rank=TRUE,mode='plot+table',prefered_colnames=c(i18n("TABEL_COLNAME_DATE"),i18n("TABEL_COLNAME_AGG"),i18n("TABEL_COLNAME_SPECIES")))
+    artfish_line_chart_server("cpue", label=i18n("SPECIES_LABEL"),df=data, colDate = "DATE",colTarget="EST_SPC_NAME",colValue="EST_LND_CPUE",ylab=i18n("CPUE_LABEL"),levels=level_choices,stat="mean", rank=TRUE,mode='plot+table',prefered_colnames=c(i18n("TABEL_COLNAME_DATE"),i18n("TABEL_COLNAME_AGG"),i18n("TABEL_COLNAME_SPECIES")))
     
     output$results<-renderUI({
     tagList(
