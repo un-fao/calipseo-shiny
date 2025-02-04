274d3654dcc0fe2096c7a7ead95c71b356680d14
eblondel <emmanuel.blondel1@gmail.com>
2025-02-04 16:01:44 +0100

    adding landing_site to sql

--- data/core/sql/artfish_A_active_vessels.sql
+++ data/core/sql/artfish_A_active_vessels.sql
@@ -1,6 +1,7 @@
 SELECT 
 YEAR as year, 
-CL_APP_MONTH_ID as month, 
+CL_APP_MONTH_ID as month,
+CL_FISH_LANDING_SITE_ID as landing_site,
 CL_FISH_FISHING_UNIT_ID as fishing_unit, 
 SUM(NB_FISHING_UNITS) as fleet_engagement_number 
 FROM dt_frame_survey 
--- data/core/sql/artfish_C_active_days.sql
+++ data/core/sql/artfish_C_active_days.sql
@@ -1,6 +1,7 @@
 SELECT 
 YEAR as year, 
-CL_APP_MONTH_ID as month, 
+CL_APP_MONTH_ID as month,
+CL_FISH_LANDING_SITE_ID as landing_site,
 CL_FISH_FISHING_UNIT_ID as fishing_unit, 
 MAX(NB_ACTIVE_DAYS) as effort_fishable_duration 
 FROM dt_frame_survey 
