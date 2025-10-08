060721bd23c7c19c5c2be125e6650a32a6fc40e4
eblondel <emmanuel.blondel1@gmail.com>
2025-10-08 17:03:36 +0200

    fix compute_fao

--- assets/country/LBN/compute_fao.R
+++ assets/country/LBN/compute_fao.R
@@ -5,8 +5,8 @@ compute_fao<-function(con=NULL,year=NULL,data){
     group_by(year)%>%
     summarise(EST_LND_CATCH= sum(catch_nominal_landed,na.rm=T)/1000)%>%
     mutate(COMMENT="",
-           EST_LND_CATCH=round(catch_nominal_landed,0))%>%
-    rename(TOTAL_LANDINGS=catch_nominal_landed,
+           EST_LND_CATCH=round(EST_LND_CATCH,2))%>%
+    rename(TOTAL_LANDINGS=EST_LND_CATCH,
            YEAR=year)%>%
     ungroup()%>%
     select(YEAR,TOTAL_LANDINGS,COMMENT)
