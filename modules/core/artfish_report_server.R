a463e36293fa3605dcd566233ef5c13879a35773
Bennici <alexandre-ben@hotmail.fr>
2026-03-04 10:21:47 +0100

    #339 adjust workflow with estimate logic

--- modules/core/artfish_report_server.R
+++ modules/core/artfish_report_server.R
@@ -14,37 +14,30 @@ artfish_report_server <- function(id, parent.session, pool, reloader){
   if(!is.null(effort_source))effort_source<-gsub("text:","",effort_source)
   minor_strata<-indicator[[1]]$compute_with$fun_args$minor_strata$source
 
-  ref_landing_site <-NULL
+  #reference data
+  ref_species <- accessRefSpecies(pool)
+  ref_species$ID <- as.character(ref_species$ID)
+  ref_fishing_units <- accessRefFishingUnits(pool)
+  ref_fishing_units$ID <- as.character(ref_fishing_units$ID)
+  ref_landing_sites<-NULL
   
   if(!is.null(minor_strata)){
     if(minor_strata=="landing_site"){
-      ref_landing_site <- accessLandingSites(pool)
-      ref_landing_site<-subset(ref_landing_site, select=c(ID,NAME))
+      ref_landing_sites <- accessLandingSites(pool)
+      ref_landing_sites<-subset(ref_landing_sites, select=c(ID,NAME))
     }
   }
   
-  #data
+  #get Artfish computation output files
+  files <-getStatPeriods(config=appConfig, "artfish_estimates",target="release+staging")
 
-  ref_species <- accessRefSpecies(pool)
-  ref_species$Species <- setNames(sprintf("%s [%s]",ref_species$NAME,ref_species$SCIENTIFIC_NAME),ref_species$ID)
-  ref_species<-subset(ref_species, select=c(ID,Species))
-  ref_fishing_unit <- accessRefFishingUnits(pool)
+  estimate <- get_artfish_results_for_ui(files, ref_fishing_units, ref_species, ref_landing_sites, with_status=T)
   
-  released_periods<-getStatPeriods(config=appConfig, "artfish_estimates",target="release")
-  if(nrow(released_periods)>0)released_periods$file_status<-"release"
-  staging_periods<-getStatPeriods(config=appConfig, "artfish_estimates",target="staging")
-  if(nrow(staging_periods)>0)staging_periods$file_status<-"staging"
-  
-  files<-rbind(released_periods,staging_periods)
-
   artfishr::artfish_shiny_report_server("artfish_report",
                                         lang = appConfig$language,
-                                        files = files,
+                                        estimate = estimate,
                                         effort_source = effort_source,
-                                        minor_strata=minor_strata,
-                                        ref_species=ref_species,
-                                        ref_fishing_unit=ref_fishing_unit,
-                                        ref_landing_site=ref_landing_site)
+                                        minor_strata=minor_strata)
   
   MODULE_END_TIME <- Sys.time()
   INFO("artfish-report: END")
